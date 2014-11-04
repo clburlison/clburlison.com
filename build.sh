5330dfd29191aa28671fe9887b86223d8a866440
Clayton Burlison <clburlison@gmail.com>
2014-11-04 05:32:43 -0600

    new build script

--- build.sh
+++ build.sh
@@ -2,14 +2,18 @@
 # enable error reporting to the console
 set -e 
 
+git config --global user.name '#{ENV['GIT_NAME']}'
+git config --global user.email '#{ENV['GIT_EMAIL']}'
+git config --global push.default simple
+
 # build site with `jekyll', by default to `_site' folder
 jekyll build
 
 # cleanup
 rm -rf ../clburlison.github.io.master
 
-#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone https://${GH_TOKEN}@github.com/clburlison/clburlison.github.io.git ../clburlison.github.io.master
+#clone `master' branch of the repository using encrypted GH_TOKEN for authentication
+git clone https://#{ENV['GIT_NAME']}:#{ENV['GH_TOKEN']}@github.com/clburlison/clburlison.github.io.git ../clburlison.github.io.master
 
 # copy generated HTML site to `master' branch
 cp -R _site/* ../clburlison.github.io.master
@@ -17,8 +21,6 @@ cp -R _site/* ../clburlison.github.io.master
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
 cd ../clburlison.github.io.master
-git config user.email "clburlison@gmail.com"
-git config user.name "Clayton Burlison"
 git add -A .
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push origin master -f
+git push --quiet origin master -f
