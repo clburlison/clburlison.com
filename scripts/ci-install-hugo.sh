fa2249276db918958795c315256453761da87d2f
Clayton Burlison <github@clburlison.com>
2017-01-13 09:44:02 -0600

    clean travis build

--- scripts/ci-install-hugo.sh
+++ scripts/ci-install-hugo.sh
@@ -1,12 +1,10 @@
 # https://github.com/bep/docuapi/blob/master/ci-install-hugo.sh
-HUGO_VERSION=0.18.1
-
 set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e $TRAVIS_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+if [ ! -e ${TRAVIS_BUILD_DIR}/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
   tar xvzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-  cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $TRAVIS_BUILD_DIR/bin/hugo
+  cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ${TRAVIS_BUILD_DIR}/bin/hugo
 fi
